4d17e0ea06002667b7afd9e8654ad9d757e445ab
Connor Cluster <Connor@MacBook-Pro.localdomain>
2022-04-30 17:10:30 -0400

    add backwards compatability for v1, broke off and updated v2 routes to align with new features

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-const productRoutes = require('./products');
 
-router.use('/products', productRoutes);
+const v1Routes = require('./v1');
+const v2Routes = require('./v2');
+
+router.use('/v1', v1Routes);
+router.use('/v2', v2Routes);
+
+router.use('/', v1Routes); // default to v1 routes so older apps don't immediately break.
+
 
 module.exports = router;
